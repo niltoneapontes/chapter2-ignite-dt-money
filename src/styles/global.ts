42c80ef6e935bf3667de67399fa712fd8f25467f
niltoneapontes <niltoneapontes@gmail.com>
2021-05-17 18:04:50 -0300

    feat: add styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,20 +20,27 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
   }
 
-  // font-size: 16px (Desktop)
-
   html {
     @media (max-width: 1080px) {
-      font-size: 93.75%; // 15px. não passamos direto os 15px por questao de acessibilidade
+      font-size: 93.75%;
     }
     @media (max-width: 720px) {
-      font-size: 87.5%; // 14px. não passamos direto os 14px por questao de acessibilidade
+      font-size: 87.5%;
     }
   }
 
   body {
     background-color: var(--background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
